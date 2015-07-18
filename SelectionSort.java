2c0af51025a51d36804081ef0f328f9b8d25a4c3
Sangeet Manghnani <sangeet.manghnani@gmail.com>
2015-07-19 01:05:40 +0530

    Selection Sort commit

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,16 +1,18 @@
+import java.util.Scanner ;
+
 public class SelectionSort {
  
     public static int[] SelectionSort(int[] arr){
          
-        for (int i = 0; i < arr.length - 1; i++)
-        {
-            int index = i;
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int min = i;
             for (int j = i + 1; j < arr.length; j++)
-                if (arr[j] < arr[index])
-                    index = j;
+                if (arr[j] < arr[min])
+                    min = j;
       
-            int smallerNumber = arr[index]; 
-            arr[index] = arr[i];
+            int smallerNumber = arr[min]; 
+            arr[min] = arr[i];
             arr[i] = smallerNumber;
         }
         return arr;
@@ -18,9 +20,21 @@ public class SelectionSort {
      
     public static void main(String a[]){
          
-        int[] arr1 = {10,34,2,56,67,88,42};
-        int[] arr2 = SelectionSort(arr1);
-        for(int i:arr2){
+	int arr1 [], size; 
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enter the size of array : ");
+	size = input.nextInt();
+	arr1 = new int[size];
+
+	System.out.println("Enter the elements of array : ");
+	for(int count = 0; count < size; count++) {
+		arr1[count] = input.nextInt();
+	}
+
+       	int[] arr2 = SelectionSort(arr1);
+       
+	for(int i:arr2){
             System.out.print(i);
             System.out.print(", ");
         }
