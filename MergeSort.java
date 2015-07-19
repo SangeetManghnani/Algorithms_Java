74dc6c41cc956f287280db0799c83b81aeda8283
Sangeet Manghnani <sangeet.manghnani@gmail.com>
2015-07-19 23:17:32 +0530

    Merge Sort

---
+++ MergeSort.java
@@ -0,0 +1,21 @@
+import java.util.Scanner ;
+
+public class MergeSort {
+
+	public static void main(String args[]){
+
+		int arr, size;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the size of the array :");
+		size = input.nextInt();
+		arr = new int[size];
+		
+		
+	}
+
+	public static void merge_sort(int array [], int low, int high) {
+	
+	//to be coded...
+	}
+}
